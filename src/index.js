611df8e0511d8132c2ce6ecf880f17eac06b789e
hacThe <19522252@gm.uit.edu.vn>
2021-08-26 23:12:55 +0700

    them vao express-handlesbars va refacter lai cau truc project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'));
+
+
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, 'resource\\views'))
+console.log(path.join(__dirname, 'resource\\views'))
+app.get('/', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
